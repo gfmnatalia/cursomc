66d88baab84564d0caacb104cbfa639999f93e11
Natalia <nataliadm@ipiranga.com.br>
2019-01-27 06:25:17 -0200

    Implementando Smtp Email Service

--- src/main/java/com/curso/jpa/config/DevConfig.java
+++ src/main/java/com/curso/jpa/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.curso.jpa.services.DBService;
+import com.curso.jpa.services.EmailService;
+import com.curso.jpa.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -30,6 +32,11 @@ public class DevConfig {
 		
 		dbService.instantiateDatabase();		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
