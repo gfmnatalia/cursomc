d6df740776069ecbf1bd1a079f2002fce49bd1b4
Natalia <nataliadm@ipiranga.com.br>
2019-01-27 04:57:27 -0200

    Implementando toString de Pedido

--- src/main/java/com/curso/jpa/domain/Pedido.java
+++ src/main/java/com/curso/jpa/domain/Pedido.java
@@ -1,8 +1,11 @@
 package com.curso.jpa.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -136,5 +139,29 @@ public class Pedido implements Serializable {
 		return true;
 	}
 
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MMM/yyyy HH:mm:ss");
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pedido numero: ");
+		builder.append(getId());
+		builder.append("\n Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append("\n Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append("\n Situação do Pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\nDetalhes:\n");
+		for (ItemPedido ip : getItens()) {
+			builder.append(ip.toString());
+		}		
+		builder.append("Valor total: ");
+		builder.append(nf.format(getValorTotal()));
+		
+		return builder.toString();
+	}
+
+	
 	
 }
--- src/main/java/com/curso/jpa/services/PedidoService.java
+++ src/main/java/com/curso/jpa/services/PedidoService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.curso.jpa.domain.ItemPedido;
 import com.curso.jpa.domain.PagamentoComBoleto;
@@ -32,6 +33,10 @@ public class PedidoService {
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+
+	@Autowired
+	private ClienteService clienteService;
+	
 	
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -39,9 +44,11 @@ public class PedidoService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));		
 	}
 	
+	@Transactional
 	public Pedido insert (Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -57,6 +64,8 @@ public class PedidoService {
 			ip.setPedido(obj);
 		}		
 		itemPedidoRepository.saveAll(obj.getItens());
+		
+		System.out.println(obj);
 		return obj;		
 	}
 	
