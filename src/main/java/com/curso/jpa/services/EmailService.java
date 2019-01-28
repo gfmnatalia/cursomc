0214d2c67e0b17dc11b317c78693ee1e9973f10c
Natalia <nataliadm@ipiranga.com.br>
2019-01-27 23:46:38 -0200

    Email HTML

--- src/main/java/com/curso/jpa/services/EmailService.java
+++ src/main/java/com/curso/jpa/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.curso.jpa.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.curso.jpa.domain.Pedido;
@@ -9,5 +11,9 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg); 
 	
 }
